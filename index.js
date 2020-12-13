c2e49317020f071a4212e850126c1153a32cccc0
Charbel t <charbel2000.52@gmail.com>
2020-12-13 05:52:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here
+class Breakfast{
+  constructor(food,drink){
+    this.food=food;
+    this.drink=drink;
+  }
+}
+class Lunch{
+  constructor(salad,soup,drink){
+    
+    this.soup=soup;
+    this.salad=salad
+  }
+}
